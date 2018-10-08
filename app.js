a32e9e1b0960b26786489a3d0a61e951a99eb458
Jonas Myrenås <jonas@myrenas.se>
2018-10-08 21:02:06 +0200

    build fixes

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'jade')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public/build')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', soundsRouter)
 app.use('/health', healthRouter)
